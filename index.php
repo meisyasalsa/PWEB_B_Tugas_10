a3cd076b6052a652fde1e2f3ce10656a10c15d09
Meisya Salsabila <112882625+meisyasalsa@users.noreply.github.com>
2022-12-15 17:47:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pendaftaran Siswa Baru | SMK Coding</title>
+</head>
+
+<body>
+	<header>
+		<h1>Pendaftaran Siswa Baru</h1>
+		<h3>oleh Meisya Salsabila Indrijo Putri</h3>
+		<h3>5025201114</h3>
+	</header>
+	
+	<h4>Menu</h4>
+	<nav>
+		<ul>
+			<li><a href="form-daftar.php">Daftar Baru</a></li>
+			<li><a href="list-siswa.php">Pendaftar</a></li>
+		</ul>
+	</nav>
+	
+	
+	<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if($_GET['status'] == 'sukses'){
+				echo "Pendaftaran siswa baru berhasil!";
+			} else {
+				echo "Pendaftaran gagal!";
+			}
+		?>
+	</p>
+	<?php endif; ?>
+	
+	</body>
+</html>
